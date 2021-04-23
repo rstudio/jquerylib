212960d4b9f5694ef165c3a9ad5ca8a1798fd76b
Carson <cpsievert1@gmail.com>
2021-04-23 09:36:45 -0500

    Use snapshot testing

---
+++ tests/testthat/_snaps/jquery.md
@@ -0,0 +1,21 @@
+# jquery_core(3) can be rendered
+
+    Code
+      as.character(script)
+    Output
+      [1] "<script src=\"lib/3.6.0/jquery-3.6.0.min.js\"></script>"
+
+# jquery_core(2) can be rendered
+
+    Code
+      as.character(script)
+    Output
+      [1] "<script src=\"lib/2.2.4/jquery-2.2.4.js\"></script>"
+
+# jquery_core(1) can be rendered
+
+    Code
+      as.character(script)
+    Output
+      [1] "<script src=\"lib/1.12.4/jquery-1.12.4.min.js\"></script>"
+
--- tests/testthat/test-jquery.R
+++ tests/testthat/test-jquery.R
@@ -1,26 +1,14 @@
-context("jquery")
-
 test_that("jquery_core(3) can be rendered", {
   script <- htmltools::renderDependencies(list(jquery_core(3, minified = TRUE)), "file")
-  expect_equal(
-    as.character(script),
-    '<script src="lib/jquery-3.5.1.min.js"></script>'
-  )
+  expect_snapshot(as.character(script))
 })
 
 test_that("jquery_core(2) can be rendered", {
   script <- htmltools::renderDependencies(list(jquery_core(2, minified = FALSE)), "file")
-  expect_equal(
-    as.character(script),
-    '<script src="lib/jquery-2.2.4.js"></script>'
-  )
+  expect_snapshot(as.character(script))
 })
 
-
-test_that("jquery_core(2) can be rendered", {
+test_that("jquery_core(1) can be rendered", {
   script <- htmltools::renderDependencies(list(jquery_core(1, minified = TRUE)), "file")
-  expect_equal(
-    as.character(script),
-    '<script src="lib/jquery-1.12.4.min.js"></script>'
-  )
+  expect_snapshot(as.character(script))
 })
